e0accefc94b5ff3b1831cc59f982e143138748d0
Benjamin <benpague@gmail.com>
2018-02-12 22:54:05 +0800

    added sqlite db in assemble.

--- assemble.py
+++ assemble.py
@@ -3,6 +3,17 @@ import settings
 import pandas as pd
 import sqlite3
 import csv
+import settings
+from dateutil.parser import parse
+
+def year(x):
+    date = parse(x)
+    return(date.year)
+
+def month(x):
+    date = parse(x)
+    return(date.month)
+
 
 def unlock_db(db_filename):
     '''Replace db_filename with the name of SQLite3 db'''
@@ -10,18 +21,13 @@ def unlock_db(db_filename):
     conn.commit()
     conn.close()
 
-def concatenate(prefix="CLAIMS"):
-    files = os.listdir(settings.DATA_DIR)
-    full = []
-    for f in files:
-        if not f.startswith(prefix):
-            continue
+def create_table():
+    df = pd.read_csv(os.path.join(settings.DATA_DIR,"claims.csv"))
+    df["YEAR"] = df["DATE_ADM"].map(lambda x: year(x))
+    df["MONTH"] = df["DATE_ADM"].map(lambda x: month(x))
+    unlock_db(os.path.join(settings.PROCESSED_DIR,"claims.db"))
+    df.to_sql(os.path.join(settings.PROCESSED_DIR, 'claims'), conn, if_exists='replace',index=False)
 
-        data = pd.read_csv(os.path.join(settings.DATA_DIR, f),index_col=False)
-        full.append(data)
+if __name__ == "__main__":
+    create_table()
 
-
-    df = pd.concat(full, axis=0)
-
-
-    df.to_csv(os.path.join(settings.PROCESSED_DIR))
